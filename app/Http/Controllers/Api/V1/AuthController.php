115447fe498452ae5c1947e330f6e31355f906c8
Yobi Bina Setiawan <yobi.studio@gmail.com>
2021-07-31 00:27:52 +0700

    add login and register

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,18 +3,36 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login';
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->input('email'))->first();
+
+        $valid = $user && Hash::check($request->input('password'), $user->password);
+
+        if ($valid) {
+            return ['token' =>  $user->createToken('nova')->accessToken];
+        }
+
+        return ['error' =>  'Invalid Credential'];
     }
 
     public function register()
     {
-        return 'register';
+        return User::updateOrCreate(
+            ['email' => 'yobi.studio@gmail.com'],
+            ['name' => 'yobi bina setiawan', 'password' => Hash::make('password')]
+        );
     }
 
     public function forgotPassword()
