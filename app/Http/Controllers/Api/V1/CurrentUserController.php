d7d9ac9e3a0429e8ca24f12feeb56139e5583d2b
Yobi Bina Setiawan <yobi.studio@gmail.com>
2021-08-01 10:10:30 +0700

    add register mail

--- app/Http/Controllers/Api/V1/CurrentUserController.php
+++ app/Http/Controllers/Api/V1/CurrentUserController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Mail\RegisterMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class CurrentUserController extends Controller
 {
     public function show()
     {
+        $user = Auth::user();
+        Mail::to($user->email)->send(new RegisterMail);
         return Auth::user();
     }
 
